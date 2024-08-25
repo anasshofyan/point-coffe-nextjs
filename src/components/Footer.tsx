0fa7ca77855cfc12ec698bf882ad4642b6664540
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-08-25 19:57:50 +0700

    chore: Add Footer component and update Navbar in MainLayout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,68 @@
+import { EnumIcons } from "@/constants/EnumIcons"
+import { Instagram, Twitter } from "lucide-react"
+import Image from "next/image"
+import React from "react"
+
+function Footer() {
+    return (
+        <footer className="w-full bg-light flex justify-between p-3 items-center text-dark">
+            <div className="max-w-screen-xl mx-auto py-10  grid-cols-5 gap-4 flex md:grid">
+                <div className="col-span-2 hidden md:block">
+                    <Image
+                        className="lg:block"
+                        src={EnumIcons.LOGO_DEFAULT}
+                        alt="logo"
+                        width={100}
+                        height={100}
+                    />
+                    <p className="text-md text-dark mt-5">
+                        ©2023 PT. INDOMARCO PRISMATAMA
+                    </p>
+                </div>
+                <div className="flex-col gap-2 hidden md:flex">
+                    <p className="text-lg text-primary font-semibold">
+                        Products
+                    </p>
+                    <p className="text-md text-dark">Coffe</p>
+                    <p className="text-md text-dark">Kopi Baper Espresso</p>
+                    <p className="text-md text-dark">Kopi Baper Robusta</p>
+                    <p className="text-md text-dark">Kopi Baper Arabica</p>
+                    <p className="text-md text-dark">Frappe Milk Series</p>
+                    <p className="text-md text-dark">Tea Series</p>
+                    <p className="text-md text-dark">Milk Indonesia</p>
+                    <p className="text-md text-dark">Hidup Sehat</p>
+                    <p className="text-md text-dark">Merchandise</p>
+                </div>
+                <div className="flex-col gap-2 hidden md:flex">
+                    <p className="text-lg text-primary font-semibold">
+                        Informations
+                    </p>
+                    <p className="text-md text-dark">Promotions</p>
+                    <p className="text-md text-dark">Fun Fact</p>
+                    <p className="text-md text-dark">Our Story</p>
+                    <p className="text-md text-dark">Feedback</p>
+                    <p className="text-md text-dark">Location</p>
+                </div>
+                <div className="flex flex-col gap-2 text-center md:text-left">
+                    <p className="text-lg text-primary font-semibold">
+                        Contact Us
+                    </p>
+                    <p className="text-md text-dark">Location</p>
+                    <p className="text-md text-dark">
+                        MENARA INDOMARET - Jalan Boulevard Pantai Indah Kapuk,
+                        Kel. Kamal Muara, Kec. Penjaringan, Kota Adm. Jakarta
+                        Utara, Provinsi DKI Jakarta, <br /> Kode Pos: 14470{" "}
+                        <br />
+                        Telp :02150897400
+                    </p>
+                    <p className="flex flex-row gap-4">
+                        <Instagram size={30} />
+                        <Twitter size={30} />
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,37 +1,13 @@
-import Button from "@/components/common/Button"
-import Navbar from "@/components/common/Navbar"
-import { EnumIcons } from "@/constants/EnumIcons"
-import { Instagram, MapPin, Menu, Twitter } from "lucide-react"
-import Image from "next/image"
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
 import { PropsWithChildren } from "react"
 
 function MainLayout({ children }: PropsWithChildren<{}>) {
     return (
         <main>
-            <Navbar>
-                <Image
-                    className="lg:block"
-                    src={EnumIcons.LOGO_WHITE}
-                    alt="logo"
-                    width={50}
-                    height={50}
-                />
-                <Menu size={30} className="block md:hidden" />
-                <div className="hidden flex-row justify-between items-center lg:flex">
-                    <Button variant="nav">Promotions</Button>
-                    <Button variant="nav">Menu</Button>
-                    <Button variant="nav">Fun Fact</Button>
-                    <Button variant="nav">Our Story</Button>
-                    <Button variant="nav">Feeback</Button>
-                    <Button variant="nav">Membership</Button>
-                </div>
-                <div className="hidden lg:flex gap-2">
-                    <Instagram size={30} />
-                    <MapPin size={30} />
-                </div>
-            </Navbar>
+            <Navbar />
             <main>{children}</main>
-            <p>footer</p>
+            <Footer />
         </main>
     )
 }
