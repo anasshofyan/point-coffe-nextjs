f38e9656bcc967b6d4b67390bfd4c4e71d89b278
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-08-25 22:14:19 +0700

    chore: Update Navbar component to include links in logo and menu buttons

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,24 +2,31 @@ import { EnumIcons } from "@/constants/EnumIcons"
 import { Instagram, MapPin, Menu } from "lucide-react"
 import Image from "next/image"
 import Button from "./Button"
+import Link from "next/link"
 
 function Navbar() {
     return (
         <nav className="fixed top-0 left-0 w-full bg-primary flex justify-between p-3 items-center text-light z-[9999]">
             <div className="max-w-screen-xl mx-auto px-5 w-full flex lg:flex-row flex-row-reverse justify-between md:flex-row md:px-5 sm:px-2 xs:px-1 items-center">
-                <Image
-                    className="lg:block"
-                    src={EnumIcons.LOGO_WHITE}
-                    alt="logo"
-                    width={50}
-                    height={50}
-                />
+                <Link href="/">
+                    <Image
+                        className="lg:block"
+                        src={EnumIcons.LOGO_WHITE}
+                        alt="logo"
+                        width={50}
+                        height={50}
+                    />
+                </Link>
                 <Menu size={30} className="block md:hidden" />
                 <div className="hidden flex-row justify-between items-center lg:flex">
                     <Button variant="nav">Promotions</Button>
-                    <Button variant="nav">Menu</Button>
+                    <Button variant="nav">
+                        <Link href="/menu">Menu</Link>
+                    </Button>
                     <Button variant="nav">Fun Fact</Button>
-                    <Button variant="nav">Our Story</Button>
+                    <Button variant="nav">
+                        <Link href="/our-story">Our Story</Link>
+                    </Button>
                     <Button variant="nav">Feeback</Button>
                     <Button variant="nav">Membership</Button>
                 </div>
