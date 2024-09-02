08ad7351d82e23f38a49020c569472191b0e3091
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-09-02 12:09:00 +0700

    refactor: Update MainLayout and _app components to use SessionWrapper for session management

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,14 @@
 import Footer from "@/components/common/Footer"
 import Navbar from "@/components/common/Navbar"
-import SessionWrapper from "@/components/SessionWrapper"
 import { PropsWithChildren } from "react"
 
 function MainLayout({ children }: PropsWithChildren<{}>) {
     return (
-        <SessionWrapper>
+        <main>
             <Navbar />
             <main>{children}</main>
             <Footer />
-        </SessionWrapper>
+        </main>
     )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,23 @@ import "react-multi-carousel/lib/styles.css"
 import { useRouter } from "next/router"
 import { ReactNode } from "react"
 import AuthLayout from "@/layouts/AuthLayout"
+import SessionWrapper from "@/components/SessionWrapper"
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter()
     const isAuthPage = router.pathname.startsWith("/auth")
 
     const getLayout = isAuthPage
-        ? (page: ReactNode) => <AuthLayout>{page}</AuthLayout>
-        : (page: ReactNode) => <MainLayout>{page}</MainLayout>
+        ? (page: ReactNode) => (
+              <SessionWrapper>
+                  <AuthLayout>{page}</AuthLayout>
+              </SessionWrapper>
+          )
+        : (page: ReactNode) => (
+              <SessionWrapper>
+                  <MainLayout>{page}</MainLayout>
+              </SessionWrapper>
+          )
 
     return getLayout(<Component {...pageProps} />)
 }
