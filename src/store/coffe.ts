859b2f60ef0c88c6e7a0d329502caa29c398af56
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-09-02 15:24:27 +0700

    refactor: Update totalCart atom to calculate quantity from cart data

--- src/store/coffe.ts
+++ src/store/coffe.ts
@@ -2,6 +2,8 @@ import { Coffee } from "@/types/coffeeTypes"
 import { atom } from "jotai"
 import { atomWithStorage } from "jotai/utils"
 
-export const cart = atom<any[]>([])
-export const totalCart = atom<number>(0)
+export const totalCart = atom<number>((get) => {
+    const cartData: Coffee[] = get(cartAtom)
+    return cartData.reduce((acc, item) => acc + item.quantity, 0)
+})
 export const cartAtom = atomWithStorage<Coffee[]>("cart", [])
