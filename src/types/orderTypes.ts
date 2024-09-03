530d176c220bde8b587d74f187b7cc9f652049a8
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2024-09-03 13:12:04 +0700

    refactor: Update CoffeeProps interface in orderTypes.ts

--- src/types/orderTypes.ts
+++ src/types/orderTypes.ts
@@ -1,5 +1,11 @@
-import { CoffeeProps } from "./coffeeTypes"
-
+interface CoffeeProps {
+    id?: number | undefined
+    name?: string | undefined
+    region?: string | undefined
+    description?: string | undefined
+    price?: number | undefined
+    image_url?: string | undefined
+}
 export interface OrderDetailsProps {
     cart: CoffeeProps[] | [] | null | undefined | any | {}
     totalAmount: number | null | undefined | any | {}
